eabfc85652e76631752f0471c560cd157fe9677f
Felix Osterbeck <cs-patzer@protonmail.com>
2025-03-22 14:40:38 +0000

    Fix isse of polybar not loading

--- dotfiles/.config/polybar/launch.sh
+++ dotfiles/.config/polybar/launch.sh
@@ -1,3 +1,10 @@
+# kill all polybar instances
+killall -q polybar
+
+# wait until the processes have been shut down
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+# start polybar for every monitor and set background with feh
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --config=~/.config/polybar/config.ini --reload epic-bar &i
     feh --bg-scale "/home/felix/Files/Wallpapers/ukyo-e_1.jpg"
