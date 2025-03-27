9dc4ff02efe9a3c674545bf69a99bb1718c91914
Felix Osterbeck <cs-patzer@protonmail.com>
2025-03-27 12:55:40 +0100

    Add telescope to nvim

---
+++ dotfiles/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,32 @@
+
+local keymap = vim.keymap
+local builtin = require("telescope.builtin")
+local actions = require("telescope.actions")
+
+local config = function()
+  local telescope = require("telescope")
+  telescope.setup {
+    defaults = {
+      mappings = {
+        i = {
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+        },
+      },
+    }
+  }
+end
+
+return {
+  'nvim-telescope/telescope.nvim', 
+  tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = config,
+  keys = {
+    keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' }),
+    keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' }),
+    keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' }),
+    keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' }),
+  },
+}
+
