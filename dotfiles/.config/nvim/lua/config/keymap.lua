009795d86ef0922ffe38bae57d702da5046f7b78
Felix Osterbeck <cs-patzer@protonmail.com>
2025-03-27 13:24:55 +0100

    Add comment plugin to nvim

--- dotfiles/.config/nvim/lua/config/keymap.lua
+++ dotfiles/.config/nvim/lua/config/keymap.lua
@@ -25,3 +25,10 @@ keymap.set("n", "<C-l>", "<C-w>l", opts) -- Navigate Right
 keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- Split Vertically
 keymap.set("n", "<leader>sh", ":split<CR>", opts) -- Split Horizontally
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts) -- Toggle Minimize
+
+--------------------------
+-- Indenting ----
+--------------------------
+
+keymap.set("v", "<", "<gv")
+keymap.set("v", ">", ">gv")
