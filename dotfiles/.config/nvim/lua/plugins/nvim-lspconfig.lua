00ec31bbda53880e70fca92f22de5f0c4b0a8e75
Felix Osterbeck <cs-patzer@protonmail.com>
2025-03-28 21:04:52 +0100

    Add lspsaga plugin to nvim

--- dotfiles/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dotfiles/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,17 +2,69 @@
 local config = function()
   local lspconfig = require("lspconfig")
 
+  ----------------------------
+  -- Signs for diagnostics ---
+  ----------------------------
   local diagnostic_signs = { Error = "", Warn = "", Hint = "ﴞ", Info = ""}
   for type, icon in pairs(diagnostic_signs) do
 		local hl = "DiagnosticSign" .. type
 		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 	end
 
+  local on_attach = function(client, bufnr)
+    -- keybind options
+    local opts = { noremap = true, silent = true, buffer = bufnr }
+
+    -- set keybinds
+    vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
+  end
 
   ----------------------------
   -- lua ---------------------
   ----------------------------
-  lspconfig.lua_ls.setup{}
+  lspconfig.lua_ls.setup {
+    on_attach = on_attach,
+    settings = { -- custom settings for lua
+      Lua = {
+        diagnostics = {
+        -- make language server recognize "vim" global
+          globals = {"vim"},
+       },
+        workspace = {
+          -- make language server aware of runtime files
+          library = {
+            vim.env.VIMRUNTIME,
+          },
+        },
+      },
+    },
+  }
+
+  local luacheck = require("efmls-configs.linters.luacheck")
+  local stylua = require("efmls-configs.formatters.stylua")
+
+  ----------------------------
+  -- configure efm server ----
+  ----------------------------
+  lspconfig.efm.setup {
+    filetypes = {
+      "lua",
+    },
+    init_options = {
+      documentFormatting = true,
+      documentRangeFormatting = true,
+      hover = true,
+      documentSymbol = true,
+      codeAction = true,
+      completion = true,
+    },
+    settings = {
+      languages = {
+        lua = { luacheck, stylua },
+      },
+    },
+  }
+
 end
 
 return {
@@ -20,6 +72,7 @@ return {
   config = config,
   dependencies = {
     "windwp/nvim-autopairs",
-    "williamboman/mason.nvim"
+    "williamboman/mason.nvim",
+    "creativenull/efmls-configs-nvim",
   }
 }
