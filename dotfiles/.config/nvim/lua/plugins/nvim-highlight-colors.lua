33df01e9f9f99b050052e1bc50faddd14d67274d
Felix Osterbeck <cs-patzer@protonmail.com>
2025-04-10 09:43:07 +0200

    Nvim: add nvim-highlight-colors plugin

---
+++ dotfiles/.config/nvim/lua/plugins/nvim-highlight-colors.lua
@@ -0,0 +1,14 @@
+
+---------------------------------------------------------------------------------------------------
+-- brenoprata10/nvim-highlight-colors"
+--
+-- Highlight colors within Neovim.
+----------------------------------------------------------------------------------------------------
+
+return {
+  "brenoprata10/nvim-highlight-colors",
+  config = function ()
+    vim.opt.termguicolors = true
+    require('nvim-highlight-colors').setup({})
+  end
+}
