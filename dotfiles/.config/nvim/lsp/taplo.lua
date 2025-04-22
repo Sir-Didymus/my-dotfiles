8e7c8502be26339659d2b9093421ca02645b0db1
Felix Osterbeck <cs-patzer@protonmail.com>
2025-04-22 23:18:02 +0200

    Nvim: add root_markers function to make taplo work in non-git repositories

--- dotfiles/.config/nvim/lsp/taplo.lua
+++ dotfiles/.config/nvim/lsp/taplo.lua
@@ -3,9 +3,22 @@
 local on_attach = require("util.lsp").on_attach
 local capabilities = require("util.capabilities").capabilities()
 
+-- IMPORTANT: this is required for taplo LSP to work in non-git repositories
+local root_markers = function(name, _)
+	local patterns = { ".git", "*.toml" }
+	for _, pattern in ipairs(patterns) do
+		if vim.glob.to_lpeg(pattern):match(name) ~= nil then
+			return true
+		end
+	end
+	return false
+end
+
 local settings = {
 	capabilities = capabilities,
 	on_attach = on_attach,
+	root_markers = root_markers,
+	filetypes = { "toml" },
 }
 
 vim.lsp.config("taplo", settings)
