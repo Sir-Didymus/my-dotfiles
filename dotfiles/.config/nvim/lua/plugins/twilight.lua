f9f411d145ac49a214ee107698efae144539b4d8
Felix Osterbeck <cs-patzer@protonmail.com>
2025-05-05 08:32:31 +0200

    Nvim: add twilight plugin

---
+++ dotfiles/.config/nvim/lua/plugins/twilight.lua
@@ -0,0 +1,26 @@
+-- folke/twilight.nvim
+--
+-- Dims inactive portions of the code you're editing.
+
+local config = function()
+	-- Set keybindings to toggle twilight
+	vim.keymap.set("n", "<leader>tw", ":Twilight<CR>", {desc="Toggle Twiglight Mode"})
+
+	require("twilight").setup({
+		context = 10, -- amount of lines we will try to show around the current line
+		treesitter = true, -- use treesitter when available for the filetype
+		-- treesitter is used to automatically expand the visible text,
+		-- but you can further control the types of nodes that should always be fully expanded
+		expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
+			"function",
+			"method",
+			"table",
+			"if_statement",
+		},
+	})
+end
+
+return {
+	"folke/twilight.nvim",
+	config = config,
+}
