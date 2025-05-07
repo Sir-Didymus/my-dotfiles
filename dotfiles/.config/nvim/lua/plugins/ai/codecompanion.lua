ef6fc91e6e16cbfd2f8240ca2deb678f361c5678
Felix Osterbeck <cs-patzer@protonmail.com>
2025-05-07 17:23:41 +0200

    Nvim: add keybinging for codecompanion plugin

--- dotfiles/.config/nvim/lua/plugins/ai/codecompanion.lua
+++ dotfiles/.config/nvim/lua/plugins/ai/codecompanion.lua
@@ -2,9 +2,16 @@
 --
 -- CodeCompanion is a productivity tool which streamlines how you develop with LLMs, in Neovim.
 
+local config = function()
+	-- Set keybinding to show codecompanion actions
+	vim.keymap.set("n", "<leader>cca", ":CodeCompanionActions<CR>", { desc = "Show CodeCompanion Actions" })
+
+	require("codecompanion").setup({})
+end
+
 return {
 	"olimorris/codecompanion.nvim",
-	opts = {},
+	config = config,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
