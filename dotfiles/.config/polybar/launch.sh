cbfa443141edaa47c13b56cabbe9fc19951a4793
Felix Osterbeck <cs-patzer@protonmail.com>
2025-06-11 22:40:45 +0200

    Add wallpapers config directory

--- dotfiles/.config/polybar/launch.sh
+++ dotfiles/.config/polybar/launch.sh
@@ -1,11 +1,11 @@
-# kill all polybar instances
+# Kill all polybar instances
 killall -q polybar
 
-# wait until the processes have been shut down
+# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-# start polybar for every monitor and set background with feh
+# Start polybar for every monitor and set background with feh
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --config=~/.config/polybar/config.ini --reload epic-bar &i
-    feh --bg-scale "/home/felix/Files/Wallpapers/ukyo-e_1.jpg"
+    feh --bg-scale "$HOME/.config/wallpapers/ukiyo-e-1.jpg"
 done
