6eeb91eca4d2dd8c4e298c71e9e79829cde8620d
Felix Osterbeck <cs-patzer@protonmail.com>
2025-06-18 09:51:29 +0200

    Nvim: disable copilot

--- dotfiles/.config/nvim/lua/plugins/ai/copilot.lua
+++ dotfiles/.config/nvim/lua/plugins/ai/copilot.lua
@@ -3,15 +3,16 @@
 -- Replacement for copilot.vim.
 
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({
-      -- It is recommended to disable copilot.lua's suggestion and panel modules,
-      -- as they can interfere with completions properly appearing in copilot-cmp.
+	"zbirenbaum/copilot.lua",
+	enabled = false, -- disables copilot by default
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		require("copilot").setup({
+			-- It is recommended to disable copilot.lua's suggestion and panel modules,
+			-- as they can interfere with completions properly appearing in copilot-cmp.
 			suggestion = { enabled = false },
 			panel = { enabled = false },
-    })
-  end,
+		})
+	end,
 }
