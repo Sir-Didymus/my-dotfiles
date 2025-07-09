cd1a3530d33e63e01c6ec22ef2cdb7643910110d
Felix Osterbeck <cs-patzer@protonmail.com>
2025-07-09 18:08:53 +0200

    Nvim: add mini.surround plugin

---
+++ dotfiles/.config/nvim/lua/plugins/mini.surround.lua
@@ -0,0 +1,10 @@
+-- echasnovski/mini.surround
+-- https://github.com/echasnovski/mini.surround
+--
+-- Neovim Lua plugin with fast and feature-rich surround actions.
+
+return {
+	"echasnovski/mini.surround",
+	opts = {}, -- for default options
+	version = false, -- use the latest version
+}
