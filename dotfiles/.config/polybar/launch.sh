1aba02b5f9b56d0122ad3536b6382cfa6572909d
Sir-Didymus <Sir-Didymus42@protonmail.com>
2025-10-21 20:20:52 +0200

    Polybar: Use primary monitor for system tray

--- dotfiles/.config/polybar/launch.sh
+++ dotfiles/.config/polybar/launch.sh
@@ -4,6 +4,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
+# Get primary monitor for tray placement
+TRAY_MONITOR=$(xrandr --query | grep " primary" | awk '{print $1}')
+export TRAY_MONITOR
+
 # Start polybar for every monitor and set background with feh
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --config=~/.config/polybar/config.ini --reload epic-bar &i
