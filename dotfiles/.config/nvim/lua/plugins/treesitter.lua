1e59041d15b19b2c99233be719f9ff934f0e49aa
Sir-Didymus <Sir-Didymus42@protonmail.com>
2026-02-21 18:13:41 +0100

    Nvim: fix treesitter config

--- dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -4,58 +4,58 @@
 -- to power faster and more accurate syntax highlighting.
 
 local config = function()
-	require("nvim-treesitter.configs").setup({
-		version = false, -- last release is way too old
-		build = ":TSUpdate",
-		autotag = { enable = false },
-		highlight = { enable = true },
-		indent = { enable = true },
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<C-s>",
-				node_incremental = "<C-s>",
-				scope_incremental = false,
-				node_decremental = "<BS>",
-			},
-		},
-		ensure_installed = {
-			"bash",
-			"c",
-			"cmake",
-			"cpp",
-			"css",
-			"comment", -- Treesitter grammar for comment tags like TODO, FIXME, etc.
-			"dart",
-			"doxygen", -- Treesitter grammar for doxygen comments.
-			"gitignore",
-			"html",
-			"http",
-			"ini",
-			"javascript",
-			"json",
-			"latex",
-			"lua",
-			"luadoc",
-			"make",
-			"markdown",
-			"markdown_inline",
-			"prolog",
-      "proto", -- Treesitter grammar for protocol buffer files.
-			"python",
-			"rust",
-			"ssh_config", -- Treesitter grammar for OpenSSH client configuration files.
-			"sql",
-			"toml",
-			"vim",
-			"vimdoc",
-			"xml",
-			"yaml",
-		},
-	})
+  require("nvim-treesitter").setup({
+    version = false, -- last release is way too old
+    build = ":TSUpdate",
+    autotag = { enable = false },
+    highlight = { enable = true },
+    indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-s>",
+        node_incremental = "<C-s>",
+        scope_incremental = false,
+        node_decremental = "<BS>",
+      },
+    },
+  })
+  require("nvim-treesitter").install({
+    "bash",
+    "c",
+    "cmake",
+    "cpp",
+    "css",
+    "comment", -- Treesitter grammar for comment tags like TODO, FIXME, etc.
+    "dart",
+    "doxygen", -- Treesitter grammar for doxygen comments.
+    "gitignore",
+    "html",
+    "http",
+    "ini",
+    "javascript",
+    "json",
+    "latex",
+    "lua",
+    "luadoc",
+    "make",
+    "markdown",
+    "markdown_inline",
+    "prolog",
+    "proto", -- Treesitter grammar for protocol buffer files.
+    "python",
+    "rust",
+    "ssh_config", -- Treesitter grammar for OpenSSH client configuration files.
+    "sql",
+    "toml",
+    "vim",
+    "vimdoc",
+    "xml",
+    "yaml",
+  })
 end
 return {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	config = config,
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  config = config,
 }
