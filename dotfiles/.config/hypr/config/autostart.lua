38328fcea9f3b0c326377baabd6807a6c4487829
Sir-Didymus <Sir-Didymus42@protonmail.com>
2026-09-19 07:45:53 +0200

    hypr: configure wallpaper

---
+++ dotfiles/.config/hypr/config/autostart.lua
@@ -0,0 +1,22 @@
+-------------------
+---- AUTOSTART ----
+-------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Autostart/
+
+-- Autostart necessary processes (like notifications daemons, status bars, etc.)
+-- Or execute your favorite apps at launch like this:
+--
+-- hl.on("hyprland.start", function ()
+--   hl.exec_cmd(terminal)
+--   hl.exec_cmd("nm-applet")
+--   hl.exec_cmd("waybar & hyprpaper & firefox")
+-- end)
+
+hl.on("hyprland.start", function()
+  hl.exec_cmd("waybar")
+  hl.exec_cmd("wl-paste --type text --watch cliphist store")
+  hl.exec_cmd("wl-paste --type image --watch cliphist store")
+  hl.exec_cmd("awww-daemon")
+  hl.exec_cmd("sh -c 'sleep 1 && awww img ~/.config/wallpapers/ukiyo-e-1-cropped.jpg --resize crop'")
+end)
--- dotfiles/.config/hypr/hyprland.lua
+++ dotfiles/.config/hypr/hyprland.lua
@@ -1,24 +1,6 @@
 require("config.monitors")
 require("config.programs")
-
--------------------
----- AUTOSTART ----
--------------------
-
--- See https://wiki.hypr.land/Configuring/Basics/Autostart/
-
--- Autostart necessary processes (like notifications daemons, status bars, etc.)
--- Or execute your favorite apps at launch like this:
---
--- hl.on("hyprland.start", function ()
---   hl.exec_cmd(terminal)
---   hl.exec_cmd("nm-applet")
---   hl.exec_cmd("waybar & hyprpaper & firefox")
--- end)
-
-hl.on("hyprland.start", function()
-	hl.exec_cmd("waybar")
-end)
+require("config.autostart")
 
 -------------------------------
 ---- ENVIRONMENT VARIABLES ----
